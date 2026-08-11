12e1428b6ad7bb24ffc1d58a0557b24454cb0626
Shin Yujin <108513967+gwonbe@users.noreply.github.com>
2026-08-11 23:15:15 +0900

    [2026.08.11] 홈에 앱 버전 표시

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:dungtak/model/song.dart';
 import 'package:dungtak/screens/game_screen.dart';
 
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -15,9 +16,24 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  String _version = "";
+
   @override
   void initState() {
     super.initState();
+    _loadVersion();
+  }
+
+  Future<void> _loadVersion() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    final version = packageInfo.version;
+
+    // 두자릿수로 맞추기
+    final parts = version.split('.');
+    final formattedVersion = parts.take(3).map((part) => part.padLeft(2, '0')).join('.');
+
+    if (!mounted) return;
+    setState(() { _version = formattedVersion; });
   }
 
   @override
@@ -34,25 +50,51 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              splashFactory: NoSplash.splashFactory,
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (_, animation, secondaryAnimation) {
-                    return GameScreen(song: song);
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            // 시작 버튼
+            Expanded(
+              child: Center(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    splashFactory: NoSplash.splashFactory,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (_, animation, secondaryAnimation) {
+                          return GameScreen(song: song);
+                        },
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    );
                   },
-                  transitionDuration: Duration.zero,
-                  reverseTransitionDuration: Duration.zero,
+                  child: const Text("START"),
                 ),
-              );
-            },
-            child: const Text("START"),
-          ),
+              ),
+            ),
+
+            // 버전
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    _version,
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
